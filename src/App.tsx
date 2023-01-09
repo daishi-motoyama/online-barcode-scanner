232e7fb3ab7aacda7548933fd81add71160d0cab
motoyama-daishi <48724872dad@gmail.com>
2023-01-09 20:50:08 +0900

    バーコード読み取り機能実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
+import { useState } from 'react'
+import { Scanner } from './Scanner'
+import { useCopyToClipboard } from 'react-use'
+
 export const App = () => {
-  return <div>Hello World!!</div>
+  const [codes, setCodes] = useState<string[]>([])
+  const [, copyToClipboard] = useCopyToClipboard()
+
+  return (
+    <>
+      <Scanner
+        onReadCode={(result) => {
+          setCodes((codes) => [...codes, result.getText()])
+        }}
+      />
+      <div>
+        <textarea style={{ height: 300 }} readOnly value={codes.join('\n')} />
+        <button type={'button'} onClick={() => copyToClipboard(codes.join('\n'))}>
+          コピー
+        </button>
+      </div>
+    </>
+  )
 }
